6121fd70d43b5f6c0479698b6d0fa06b54963e54
qian <1962968168@qq.com>
2019-05-12 00:17:13 +0800

    后台首页静态页面布局完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -10,4 +10,30 @@ $(document).ajaxStop(function () {
 
     NProgress.done();
     
-})
+});
+
+$(function () {
+
+ //点检侧边导航.展开导航
+    $(".category").on("click",function () {
+
+        $(".child").stop().slideToggle();
+
+    })
+
+    //点击导航菜单，导航隐藏
+    $(".icon-menu").on("click",function () {
+
+        $(".aside").stop().toggleClass("hidemenu");
+        $(".main").stop().toggleClass("hidemenu");
+        $('.topbar').stop().toggleClass("hidemenu");
+        
+    })
+
+    //点击退出按钮，模态框出现
+    $(".icon-logout").on("click",function () {
+        $('#Modal').modal('show')
+
+    })
+
+});
