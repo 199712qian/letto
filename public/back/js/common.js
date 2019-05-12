13e3b6410b326edda00cd04271469dfcf9ca570d
qian <1962968168@qq.com>
2019-05-12 16:41:56 +0800

    后台首页图表功能完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -12,6 +12,30 @@ $(document).ajaxStop(function () {
     
 });
 
+//登录拦截功能，判断当前用户是否登录，如果用户已经登录，则让用户继续浏览页面，如果用户没用登录，则拦截到登录页
+//判断当前页是否是登录页，如果是登录页，就不用进行登录拦截
+if(location.href.indexOf("login.html")=== -1)
+{
+    $.ajax({
+        type:"get",
+        url:"/employee/checkRootLogin",
+        dataType: "json",
+        success:function (info) {
+
+            if(info.success)
+            {
+                console.log("已经登录");
+            }
+            if(info.error === 400)
+            {
+                location.href="login.html";
+            }
+
+        }
+    })
+}
+
+
 $(function () {
 
  //点检侧边导航.展开导航
@@ -30,9 +54,30 @@ $(function () {
         
     })
 
-    //点击退出按钮，模态框出现
+    //点击模态框按钮，模态框出现
     $(".icon-logout").on("click",function () {
         $('#Modal').modal('show')
+
+    })
+
+    //点击退出按钮，实现退出登录状态
+    $("#btn-logout").on("click",function () {
+
+        $.ajax({
+            type:"get",
+            url:"/employee/employeeLogout",
+            dataType:"json",
+            success:function (info) {
+
+                console.log(info);
+
+                if(info.success)
+                {
+                    location.href="login.html";
+                }
+
+            }
+        })
 
     })
 
